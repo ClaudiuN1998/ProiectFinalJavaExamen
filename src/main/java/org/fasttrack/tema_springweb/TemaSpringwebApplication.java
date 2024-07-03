e7f7e5da8ee19d67032b4b38ed19ba95937cc145
Nastasie Claudiu <id_claudiu@yahoo.com>
2024-07-03 10:47:43 +0300

    Tema curs 17

---
+++ src/main/java/org/fasttrack/tema_springweb/TemaSpringwebApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrack.tema_springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemaSpringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemaSpringwebApplication.class, args);
+	}
+
+}
